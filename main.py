97e230a2890caf231afcf5e4c37eb50873317bb4
ekrsw <eisuke.koresawa@gmail.com>
2025-02-28 12:23:50 +0900

    password

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from models.user import User
 
 
 async def main():
-    await User.create_user("test_user")
+    await User.create_user("test_user", "password", False)
+    users = await User.get_all_users()
+    for user in users:
+        print(f"username: {user.username}, password: {user.hashed_password}, is_admin: {user.is_admin}")
 
 if __name__ == "__main__":
     asyncio.run(main())
--- models/user.py
+++ models/user.py
@@ -1,16 +1,33 @@
 from fastapi import HTTPException
-from sqlalchemy import Column, String, Select
+from passlib import CryptContext
+from sqlalchemy import Boolean, Column, String, Select
+from typing import Optional
 from .base import BaseDatabase
 from db.session import AsyncContextManager
+
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 class User(BaseDatabase):
     __tablename__ = "users"
     username = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String)
+    is_admin = Column(Boolean, default=False)
+
+    async def valify_password(self, plain_password: str) -> bool:
+        """入力されたパスワードがハッシュと一致するかを確かめる"""
+        return pwd_context.verify(plain_password, self.hashed_password)
     
     @classmethod
-    async def create_user(cls, username: str):
+    async def set_password(cls, plain_password: str):
+        """パスワードをハッシュ化して保存する"""
+        cls.hashed_password = pwd_context.hash(plain_password)
+    
+    @classmethod
+    async def create_user(cls, username: str, plain_password: str, is_admin: bool):
         async with AsyncContextManager() as session:
-            session.add(cls(username=username))
+            hashed_password = await cls.set_password(plain_password)
+            session.add(cls(username=username, hashed_password=hashed_password, is_admin=is_admin))
     
     @classmethod
     async def get_all_users(cls):
@@ -38,10 +55,12 @@ class User(BaseDatabase):
             return user
     
     @classmethod
-    async def update_user(cls, user_id: int, username: str):
+    async def update_user(cls, user_id: int, username: str, is_admin: Optional[bool]=None):
         async with AsyncContextManager() as session:
             user = await cls.get_user_by_id(user_id)
             user.username = username
+            if is_admin is not None:
+                user.is_admin = is_admin
             session.add(user)
     
     @classmethod
@@ -49,3 +68,11 @@ class User(BaseDatabase):
         async with AsyncContextManager() as session:
             user = await cls.get_user_by_id(user_id)
             await session.delete(user)
+    
+    @classmethod
+    async def update_password(cls, user_id: int, plain_password: str):
+        async with AsyncContextManager() as session:
+            user = await cls.get_user_by_id(user_id)
+            hashed_password = await cls.set_password(plain_password)
+            user.hashed_password = hashed_password
+            session.add(user)
