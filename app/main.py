ae0eb3c50140fa6b85fbcd0d6f1572507047c131
ekrsw <eisuke.koresawa@gmail.com>
2025-03-05 21:39:13 +0900

    create_user endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from app.models.user import User
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello aaa"}
+    return {"message": "Hello bbb"}
+
+@app.post("/users/")
+async def create_user(username: str, password: str, is_admin: bool=False):
+    exit_user = await User.get_user_by_username(username)
+    if exit_user:
+        return {"message": "User already exists"}
+    new_user = await User.create_user(username, password, is_admin)
+    return new_user
