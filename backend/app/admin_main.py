68fdab83853b60148483d77cf7b43c7c59fa2db4
ekrsw <eisuke.koresawa@gmail.com>
2025-03-14 17:00:08 +0900

    user_csv_import views

--- backend/app/admin_main.py
+++ backend/app/admin_main.py
@@ -5,6 +5,7 @@ from sqladmin import Admin
 
 from app.admin.core.auth import AdminAuth
 from app.admin.views.users import UserAdminView
+from app.admin.views.user_csv_import import UserCsvImportAdminView
 from app.core.config import settings
 from app.db.database import Database
 
@@ -29,9 +30,11 @@ admin_manager = Admin(
     app,
     title="管理画面",
     session_maker=async_session_factory,  # SQLAlchemyのセッションを作成する関数
+    templates_dir="app/admin/templates",  # Jinja2テンプレートのディレクトリ
     # 認証用のクラスを指定
     authentication_backend=AdminAuth(secret_key=settings.jwt_secret_key),
 )
 
-# 管理画面のviewを追加する
+# viewを追加する
 admin_manager.add_view(UserAdminView)
+admin_manager.add_view(UserCsvImportAdminView)
