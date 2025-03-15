9410616d402b101d421fa1f682bb0e663db534d5
ekrsw <eisuke.koresawa@gmail.com>
2025-03-15 21:47:39 +0900

    postgres setup

--- backend/docker/postgresql/init/init-db.sh
+++ backend/docker/postgresql/init/init-db.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-# PostgreSQLのデフォルトデータベース（postgres）に接続
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
-    -- my_databaseが存在しない場合のみ作成
-    SELECT 'CREATE DATABASE my_database' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'my_database')\gexec
-    GRANT ALL PRIVILEGES ON DATABASE my_database TO $POSTGRES_USER;
+psql -v ON_ERROR_STOP=1 --username "$DATABASE_USER" --dbname "postgres" <<-EOSQL
+    SELECT 'CREATE DATABASE ${DATABASE_NAME}' 
+    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${DATABASE_NAME}')\gexec
+    
+    GRANT ALL PRIVILEGES ON DATABASE "${DATABASE_NAME}" TO ${DATABASE_USER};
 EOSQL
